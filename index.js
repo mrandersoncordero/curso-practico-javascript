740f783472808bff1e11ce49699b6be944fc9e9f
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-05-10 20:23:49 -0400

    usar addEventListener y crear minu calculadora

--- index.js
+++ index.js
@@ -3,37 +3,16 @@
 // #pid { ... }
 
 const h1 = document.querySelector('h1');
-const p = document.querySelectorAll('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
-console.log(input.value);
+btn.addEventListener('click', (e) => {
+    num1 = parseInt(input1.value);
+    num2 = parseInt(input2.value);
+    resultado = num1 + num2;
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
+    e.preventDefault();
+    pResult.innerHTML = 'Resultado: ' + resultado;
 });
-
-
-h1.innerHTML = 'El HAHAS <br> Alias Joker'; // innerHTML inserta HTML en tu HTML reconociendo las etiquetas
-h1.innerText = 'El HAHAS <br> Alias Joker' // Inserta Texto plano en tu HTML
-// console.log(h1.getAttribute('class'))
-// h1.setAttribute('class', 'rojo')
-
-h1.classList.add('rojo');
-h1.classList.remove('rojo');
-// h1.classList.toggle('verde');
-// h1.classList.contains('verde');
-
-input.value = '456';
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://imageboss.me/integrations/javascript-logo.svg');
-console.log(img);
-
-pid.innerHTML = "";
-pid.appendChild(img);
